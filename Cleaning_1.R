d8e64a537976462f5a99bdf984771093db8033cb
Melody Chen <ychen03@wesleyan.edu>
2018-02-22 18:59:20 -0500

    w/ location

--- Cleaning_1.R
+++ Cleaning_1.R
@@ -3,3 +3,7 @@
 winter = read_csv("/Users/yanjunchen/Desktop/Visual Data/winter.csv")
 US_Gold = subset(winter, Country == "USA" & Medal == "Gold")
 write.csv(US_Gold, "US_Gold.csv")
+
+location = read_xlsx("/Users/yanjunchen/Desktop/Visual Data/location.xlsx")
+#add locations by merging two datasets by "City"
+newdata = merge(US_Gold, location[, c("City","Location")], by = "City")
